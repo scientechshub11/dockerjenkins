100f6a32834509e8d51f216dec82f83da5d5da49
Anwesha Chakraborty <anweshachakraborty903@gmail.com>
2023-11-25 22:50:09 +0530

    commited changes for jenkins pipeline

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, ()=>{
+    console.log(`app listen to the port ${port}`)
+})
+
+app.get('/', (req, res)=>{
+    res.send("page loaded successfully!!!!!")
+})
